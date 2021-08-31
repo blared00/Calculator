7d2139c041bb9d70dc21605383e79229bfd9c40a
blared00 <yudintsev.vsevolod@gmail.com>
2021-08-31 13:37:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+class Calculator:
+    pass
+
+
+class CaloriesCalculator(Calculator):
+    pass
+
+
+class CashCalculator(Calculator):
+    pass
+
+
+class Record:
+    pass
+
+
+if __name__ == '__main__':
+    pass
