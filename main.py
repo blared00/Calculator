bb614763671cac0ba24ceeb8d4031d30563bd10f
blared00 <blared009@list.ru>
2021-08-31 21:17:23 +0300

    Написание валидации входных полей. Корректировка логики работы функций "остатка на день"

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ class Calculator:
     """Базовый калькулятор."""
 
     def __init__(self, limit):
-        self.limit = limit
+        if isinstance(limit, int):
+            self.limit = limit
+        else:
+            raise ValueError('Лимит должен быть целым числом')
         self.records = []
 
-    def add_record(self, amount, comment, date=datetime.now().date()):
+    def add_record(self, record):
         """Сохраняет новую запись о расходах."""
-        record = Record(amount, comment, date)
-        self.records += record
+        if isinstance(record, Record):
+            self.records.append(record)
+        else:
+            raise ValueError('Для внесения записи воспользуйтесь объектом Record')
 
     def get_today_stats(self, date_counting=datetime.now().date()):
         """Подсчет за день."""
         result = []
         for record_today in self.records:
-            if record_today.date == date_counting:
+            if record_today.date.day == date_counting.day and record_today.date.month == date_counting.month and record_today.date.year == date_counting.year:
                 result.append(record_today.amount)
         return sum(result)
 
@@ -32,33 +37,101 @@ class CaloriesCalculator(Calculator):
 
     def get_calories_remained(self):
         """Определяет, сколько ещё калорий можно/нужно получить сегодня"""
+        remains = self.limit - self.get_today_stats()
         if self.get_today_stats() < self.limit:
-            remains = self.get_today_stats() - self.limit
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {remains} кКал'
         return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
     """Калькулятор подсчета денег"""
-    USD_RATE = 1
-    EURO_RATE = 1
+    USD_RATE = 73.32
+    EURO_RATE = 86.50
+    CURRENCY_LIST = {'руб': 'руб',
+                     'rub': 'руб',
+                     'usd': 'USD',
+                     'euro': 'Euro'}
 
     def get_today_cash_remained(self, currency):
-        """Определет, сколько ещё денег можно потратить сегодня в рублях, долларах или евро."""
-        if self.get_today_stats() < self.limit:
-            remains = self.get_today_stats() - self.limit
-            return f'На сегодня осталось {remains} {currency}'
-        elif self.get_today_stats() > self.limit:
-            return 'Денег нет, держись: твой долг - N руб/USD/Euro'
-        return 'Денег нет, держись'
+        """Определяет, сколько ещё денег можно потратить сегодня в рублях, долларах или евро."""
+        if currency in self.CURRENCY_LIST.keys():
+            currency = self.CURRENCY_LIST[currency.lower()]
+            remains = self.limit - self.get_today_stats()
+            if self.get_today_stats() < self.limit:
+                return f'На сегодня осталось {remains} {currency}'
+            elif self.get_today_stats() > self.limit:
+                return f'Денег нет, держись: твой долг - {-remains} {currency}'
+            return 'Денег нет, держись'
+        return 'Такая валюта не поддерживается'
 
 
 class Record:
-    def __init__(self, amount, comment, date):
-        self.amount = amount
+    """Создание записи о потреблении """
+    def __init__(self, amount, comment, date=datetime.now().date().strftime('%d.%m.%Y')):
+        if isinstance(amount, int):
+            self.amount = amount
+        else:
+            raise ValueError('Количество должно быть целым числом')
         self.comment = comment
-        self.date = datetime.strptime(date, '%d.%m.%Y')
+        try:
+            self.date = datetime.strptime(date, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError('Неверный формат даты. Введите ДД.ММ.ГГ')
 
 
 if __name__ == '__main__':
-    pass
+    cash_calculator = CashCalculator(1000)
+
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    cash_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    cash_calculator.add_record(Record(amount=3000,
+                                      comment='бар в Танин др',
+                                      date='29.08.2021'))
+
+
+    print(cash_calculator.get_today_cash_remained('rub'))
+    # должно напечататься
+    # На сегодня осталось 555 руб
+    cash_calculator.add_record(Record(amount=555,
+                                      comment='бар в Танин др',
+                                      ))
+    print(cash_calculator.get_today_cash_remained('rub'))
+    # должно напечататься
+    # Денег нет, держись
+    cash_calculator.add_record(Record(amount=555,
+                                      comment='бар в Танин др',
+                                      ))
+    print(cash_calculator.get_today_cash_remained('rub'))
+    # должно напечататься
+    # Денег нет, держись: твой долг - 555 руб
+
+
+
+    calories_calculator = CaloriesCalculator(1000)
+
+    # дата в параметрах не указана,
+    # так что по умолчанию к записи
+    # должна автоматически добавиться сегодняшняя дата
+    calories_calculator.add_record(Record(amount=145, comment='кофе'))
+    # и к этой записи тоже дата должна добавиться автоматически
+    calories_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+    # а тут пользователь указал дату, сохраняем её
+    calories_calculator.add_record(Record(amount=3000,
+                                      comment='бар в Танин др',
+                                      date='29.08.2021'))
+
+
+    print(calories_calculator.get_calories_remained())
+    # должно напечататься
+    # Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 555 кКал
+    calories_calculator.add_record(Record(amount=556,
+                                          comment='бар в Танин др',
+                                          ))
+    print(calories_calculator.get_calories_remained())
+    # должно напечататься
+    # Хватит есть!
