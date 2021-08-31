ac87dc068bda1e4e7f6384983a4d5d6bd27ee171
blared00 <yudintsev.vsevolod@gmail.com>
2021-08-31 14:23:33 +0300

    Создание базовых классов без логики функций.

--- main.py
+++ main.py
@@ -2,19 +2,45 @@ from datetime import datetime
 
 
 class Calculator:
-    pass
+    """Базовый калькулятор."""
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, amount, comment, date=datetime.now().date()):
+        """Сохраняtn новую запись о расходах."""
+        record = Record(amount, comment, date)
+        self.records += record
+
+    def get_today_stats(self):
+        """Подсчет за сегодня."""
+        pass
+
+    def get_week_stats(self):
+        """Подсчет за неделю."""
+        pass
 
 
 class CaloriesCalculator(Calculator):
-    pass
+    """Калькулятор калорий"""
+    def get_calories_remained(self):
+        """Определять, сколько ещё калорий можно/нужно получить сегодня"""
+        pass
 
 
 class CashCalculator(Calculator):
-    pass
+    """Калькулятор подсчета денег"""
+    def get_today_cash_remained(self, currency):
+        """Определет, сколько ещё денег можно потратить сегодня в рублях, долларах или евро."""
+        pass
 
 
 class Record:
-    pass
+    def __init__(self, amount, comment, date):
+        self.amount = amount
+        self.comment = comment
+        self.date = datetime.strptime(date, '%d.%m.%Y')
+
 
 
 if __name__ == '__main__':
